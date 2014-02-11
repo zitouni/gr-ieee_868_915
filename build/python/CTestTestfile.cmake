ff449ee2f3538fc8f99d1bd3022b024d0ec8269c
Bastian Bloessl <bloessl@ccs-labs.org>
2014-02-11 12:20:59 +0100

    del build dir

--- build/python/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zitouni/gnuradio-3.7.2/gr-ieee_868_915/python
-# Build directory: /home/zitouni/gnuradio-3.7.2/gr-ieee_868_915/build/python
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(qa_symbols_to_chips_bs "/bin/sh" "/home/zitouni/gnuradio-3.7.2/gr-ieee_868_915/build/python/qa_symbols_to_chips_bs_test.sh")
